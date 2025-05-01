b9e0aeb44213a7ef798c9e71e811c261eaa87e42
IceCreamCatLol <181563521+IceCreamCatLol@users.noreply.github.com>
2025-05-01 23:25:53 +0300

    Create roblox sound player.lua

---
+++ roblox sound player.lua
@@ -0,0 +1,118 @@
+-- script made by @ShutUpJamesTheLoser
+-- this is not deobfuscated version, this is dumped version which means you can see some functions but not the script
+-- this wont work if you'll try running it in executors
+-- yes you can understand almost anything, im sorry i tried
+
+
+stringcharbytesubbit32bitbxortableconcatinsert@j@)O,ݹ@ j@YֵC
+xxܭB+,{i@Oj\,i@	ȟՏqټi@	jY^qƮ/k96+h@sWV:6:h@}0zIS 8@:`h@	BqAej.h@)ޒZg@9] g@	[mSf@
+fc"bI&d'Uf@K꺬Pq$֋@f@	
+MMQe?&!e@64Ǥsg`e@CpE,dq`d@nͽp@c@
+.蠤zb@'Lr`b@$7\9,IqPX.W`a@o@`@
+҈-ɤXn_@kQƞ^@˪؉\@
+hR0gMPr8>eIGZ@
+HH<Z@	e'J6!rr6H?XE>@X@2ǖg@W@N@y@U@	[e^p*~4tS@3?%KgvP@
+3Z7QR=O@/1#Ң/0։JxS6@9mflxl>2@aRP~۹"=$@-,IL@*2#62Wyʫ\G@	98jbJbgamePlayersLocalPlayerGetMouseInstancenew	PlayerGuiNameResetOnSpawnPositionUDim2(\?x?Sizew@n@BackgroundColor3Color3fromRGBo@CornerRadiusUDim @	Thickness@Active	Draggable
+InputBeganConnectInputChanged
+GetService㥛 ? rh?s@l@Imagerbxassetid://114287843280734BackgroundTransparency?~jt?B`"? v@I@?Text
+TextColor3TextSizeH@FontEnumSourceSansBold	FocusLostv/?!rh??D@@sh|???@P@B@;@3@pairsGetDescendantsIsAMouseButton1Clicko4B PB PB PBZ8B PB P	B P
+NNNNNN/	(
+(
+:	2	/	(
+(:	2	/	(
+(:	2	/	(
+(:	2	/	(
+(:	2	/	(
+(:	2	/	(
+(:	2	/	(
+!(":	2 	/	(
+$(%:	2#	/	(
+'((:	2&	/	(
+*(+:	2)	/	(
+-(.:	2,	/	(
+0(1:	2/	/	(
+3(4:	22	/	(
+6(7:	25	/	(
+9(::	28	/	(
+<(=:	2;	/	(
+?(@:	2>	/	(
+B(C:	2A	/	(
+E(F:	2D	/	(
+H(I:	2G	/	(
+K(L:	2J	/	(
+N(O:	2M	/	(
+Q(R:	2P	/	(
+T(U:	2S	/	(
+W(X:	2V	/	(
+Z([:	2Y	/	(
+](^:	2\	/	(
+`(a:	2_	/	(
+c(d:	2b	/	(
+f(g:	2e	/	(
+i(j:	2h	/	(
+l(m:	2k	/	(
+o(p:	2n	/	(
+r(s:	2q	/	(
+u(v:	2t	/	(
+x(y:	2w	/	(
+{(|:	2z	B	} P		~ P		 M
+	
+B P Pz P
+	:
+ Pw20B P P
+t/:B
+ P
+
+((((:
+2
+B
+ P
+
+((((:
+2
+B
+ P
+
+(((w:
+2
+B
+ P
+
+ Pq/:
+B P((:2
+B P Pn/:0005NNN P MNNNN# P MN#B} M Pk: P MNNN#B P Ph/:B P((((:2B P((((:200B P Pe/:B P((((:2B P((((:2B P(((:20 Pb2B P(((:20B P P2B P P_/:B P((:2B P P\/:0 P MNN#B P PV/:B P((((:2B P((((:2B P(((:20 PS2B P(((:20B P P2B P PP/:B P((:2B P PM/:0B P PJ/:B P(((( : 2B P(((( : 2B P(((:20 PG2B P(((:20B P P2B P PD/:B P((:2B P PA/:0B P P>/ : B P( (!("(#:#2B P( (!("(#:#2B P( (!(":"20 P;2B P( (!(":"20B P P2B P P 8/!:!B  P  (!(": "2 B  P   P!5/": "0 B!} M!! P#2:!#4"B# M$!Y$%1#%8W M(' P*/:(*&(W8W P(' P),'(R)8R P(' P))K(W)8WB( P((
+/)"/*'#(*#E8E P# M##%NN"NN!##% P# M##%N"N##% P# M##%N"N##%		?p@&4(-(!O/O	O
+OO/
+/ D[$OO
+/-S-S D[
+	$
+ 0
+
+Y	
+QO/JPositionUDim2newXScaleOffsetY(5&8 POOB PO P PO P P PO P PO P P P		:288	
+UserInputTypeEnumMouseButton1Touch?PositionChangedConnect( PB P P'8 PB P PK'8'(5&8(&8O PR P M	NA#8'&8HR PR(88'8UserInputStateEnumEnd
+O PB P PK	8	HR
+UserInputTypeEnum
+MouseMovementTouch PB P P'8 PB P PK
+8
+R
+OK	8	O&	8	O/TextZ@	O P&8OO P2Texttonumberpairs
+FireServerg@h@
+rbxassetid://?$@`h@h@h@waiti@@(5&8O PB/&*8*BO38'(&8 MO
+ P
+
+O PO(
+/
+
+(	(
+H# MO
+ P
+
+O P#8'888?(&585OO P
+2B(	(	U	+8+OO P28?8+8?8pairs
+FireServeri@i@
+BO38
+ MO PO P#8pairs
+FireServer j@@j@
+BO38
+ MO PO P#8
